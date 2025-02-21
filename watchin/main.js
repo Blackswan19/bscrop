8096793f75079ce6bc93420336c8a9b5c4ed6a34
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-02-21 19:06:25 +0530

    Update main.js

--- watchin/main.js
+++ watchin/main.js
@@ -144,3 +144,19 @@ function closeAddMoviePopup() {
 window.onload = function () {
     displayStoredMedia();
 };
+document.addEventListener("DOMContentLoaded", () => {
+    const customMenu = document.querySelector(".custom-menu");
+
+    // Show custom menu on right-click
+    document.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+        customMenu.style.display = "block";
+        customMenu.style.top = `${event.pageY}px`;
+        customMenu.style.left = `${event.pageX}px`;
+    });
+
+    // Hide the menu when clicking elsewhere
+    document.addEventListener("click", () => {
+        customMenu.style.display = "none";
+    });
+});
